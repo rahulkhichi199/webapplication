d682d09aa82446a6c34e7f8a8e64465a694caa50
rahulkhichi199 <rahulkhichi331@gmail.com>
2022-04-24 18:09:30 +0530

    apiss

--- src/component/UploadNFT.jsx
+++ src/component/UploadNFT.jsx
@@ -4,11 +4,64 @@ import './UploadNFT.css';
 const http = require('http');
 
 
+
 export class UploadNFT extends Component {
+
+
+    // constructor()
+    // {   
+    //     super();
+    //     this.state={value:''};
+    // }
+    // charallow(e)
+    // {
+    //     this.setState({inputtxt:e.target.value.replace(/[^a-zA-Z]/ig,'')})
+    // }
+
+constructor(props){
+    super(props);
+    this.state ={
+        NFTname: '',
+        Title: '',
+        Amount: '',
+        AssetID: '',
+        Tags: '',
+        Creatorname: '',
+    };
+}
+
+handleInputChange = e =>{
+    this.setState({
+        [e.target.name]:e.target.value,
+    });
+};
+
+handleSubmit = e =>{
+    e.preventDefault();
+    const { NFTname, Title, Amount, AssetID, Tags, Creatorname} = this.state;
+    const NFT = {
+        NFTname,
+        Title,
+        Amount,
+        AssetID,
+        Tags,
+        Creatorname,
+    };
+axios
+.post('http://localhost:5000/createnft',NFT)
+.then(()=> console.log('NFT created'),
+
+
+)
+.catch(err =>{
+    console.err(err);
+});
+};
 
 
     state = {
         profileImg: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+
     }
     imageHandler = (e) => {
         const reader = new FileReader();
@@ -23,24 +76,24 @@ export class UploadNFT extends Component {
         const { profileImg } = this.state
 
         // form submit steps
-        const submitHandler = (event) => {
-            event.preventDefault();
-            // const image = event.target.image - upload.value;
-            const NFTname = event.target.NFTname.value;
-            const Title = event.target.Title.value;
-            const Amount = event.target.Amount.value;
-            axios.post("https://jsonplaceholder.typicode.com/posts", {
-                NFTname,
-                Title,
-                Amount
-            })
-                .then(response => {
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        };
+        // const submitHandler = (event) => {
+        //     event.preventDefault();
+        //     // const image = event.target.image - upload.value;
+        //     const NFTname = event.target.NFTname.value;
+        //     const Title = event.target.Title.value;
+        //     const Amount = event.target.Amount.value;
+        //     axios.post("http://localhost:5000/nft", {
+        //         NFTname,
+        //         Title,
+        //         Amount
+        //     })
+        //         .then(response => {
+        //             console.log(response);
+        //         })
+        //         .catch((error) => {
+        //             console.log(error);
+        //         });
+        // };
 
 
         return (
@@ -49,7 +102,7 @@ export class UploadNFT extends Component {
 
             <div>
 
-                <form class="row g-3" onSubmit={submitHandler}>
+                <form class="row g-3 needs-validation"  onSubmit={this.handleSubmit} checkValidity>
 
                     <div className="page" id='page'>
                         <div className="container" id='container'>
@@ -57,7 +110,7 @@ export class UploadNFT extends Component {
                             <div className="img-holder" id='img-holder'>
                                 <img src={profileImg} alt="" id="img" className="img" />
                             </div>
-                            <input type="file" accept="image/*" name="image-upload" id="input" onChange={this.imageHandler} />
+                            <input type="file" accept=".png, .jpeg" name="image-upload" id="input" onChange={this.imageHandler} />
                             <div className="label" id='label'>
                                 <label className="image-upload" id='image-upload' htmlFor="input">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image-fill" viewBox="0 0 16 16">
@@ -78,42 +131,42 @@ export class UploadNFT extends Component {
 
                     <div class="col-md-4">
                         <label for="validationServer01" class="form-label">NFT name</label>
-                        <input type="text" name='NFTname' class="form-control " id="validationServer01" required />
+                        <input type="text" name='NFTname' class="form-control " id="validationServer01" onChange={this.handleInputChange} required />
                         <div class="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                     <div class="col-md-4">
                         <label for="validationServer02" class="form-label">Title</label>
-                        <input type="text" name='Title' class="form-control " id="validationServer02" required />
+                        <input type="text" name='Title' class="form-control " id="validationServer02" onChange={this.handleInputChange}  required/>
                         <div class="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                     <div class="col-md-4">
                         <label for="validationServer02" class="form-label">Amount</label>
-                        <input type="text" name='Amount' class="form-control " id="validationServer02" required />
+                        <input type="number"  name='Amount' class="form-control "  step='any' id="validationServer02" onChange={this.handleInputChange} required />
                         <div class="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                     <div class="col-md-4">
                         <label for="validationServer02" class="form-label">Asset ID</label>
-                        <input type="text" name='Asset ID' class="form-control " id="validationServer02" required />
+                        <input type="text" name='AssetID' class="form-control " id="validationServer02" onChange={this.handleInputChange} required />
                         <div class="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                     <div class="col-md-8">
                         <label for="validationServer02" class="form-label">Tags</label>
-                        <input type="text" name='Tags' class="form-control " id="validationServer02" required />
+                        <input type="text" name='Tags' class="form-control " id="validationServer02" onChange={this.handleInputChange} required />
                         <div class="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                     <div class="col-md-6">
                         <label for="validationServer02" class="form-label">Creator name</label>
-                        <input type="text" name='Creator name' class="form-control " id="validationServer02" required />
+                        <input type="text" name='Creatorname' class="form-control " id="validationServer02"   onChange={this.handleInputChange} required />
                         <div class="valid-feedback">
                             Looks good!
                         </div>
